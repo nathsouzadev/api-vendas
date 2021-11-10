a777d49336c1ae2ac63db28db63e7f200919c3d1
Nathally Souza <nathallyccd@gmail.com>
2021-11-09 21:54:08 -0300

    Initialize project

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import 'reflect-metadata';
+import express, { Response, Request, NextFunction } from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if(error instanceof Error){
+        return response.status(400).json({
+            error: error.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+});
+
+app.use(function(request: Request, response: Response, next: NextFunction) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.json());
+
+export { app }
